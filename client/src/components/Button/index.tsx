174a3171c55a5949dab0a6512f949c3c674cabb4
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2021-05-18 01:01:52 +0300

    add more props to button

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,13 +1,40 @@
+import clsx from 'clsx'
 import React from 'react'
 
 type Props = {
   label: string
+  size?: 'sm' | 'md' | 'lg'
   onClick?: (p: any) => void
+  color?: string
 }
 
-const Button = ({ label, onClick }: Props) => {
+const Button = ({ label, onClick, size = 'md', color }: Props) => {
+  let buttonSize
+
+  switch (size) {
+    case 'sm': {
+      buttonSize = 'px-4 py-1'
+      break
+    }
+
+    case 'md': {
+      buttonSize = 'px-6 py-2'
+      break
+    }
+
+    case 'lg': {
+      buttonSize = 'px-8 py-3'
+      break
+    }
+
+    default:
+      buttonSize = 'px-6 py-2'
+  }
+
+  const buttonClass = clsx('border-2', buttonSize, `bg-${color}`)
+
   return (
-    <button className='border-2 px-6 py-1 bg-green-500' onClick={onClick}>
+    <button className={buttonClass} onClick={onClick}>
       {label}
     </button>
   )
